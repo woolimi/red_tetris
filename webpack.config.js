5a4f2b03dff5536964f447aef9923cc545e41066
wpark <woolimi91@naver.com>
2020-10-23 22:58:12 +0200

    project webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+	name: 'red-tetris',
+	mode: 'development', // production
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+	entry: './src/client/index.js',
+	module: {
+		rules: [{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ["@babel/preset-env", "@babel/preset-react"],
+					plugins: ["@babel/plugin-proposal-class-properties"]
+				}
+			}
+		},
+		{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		},
+		{
+			test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+			loader: 'url-loader',
+			options: {
+				limit: 100000,
+			},
+		}
+		]
+	},
+	output: {
+		path: path.join(__dirname, outputDirectory),
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './public/index.html'
+			// favicon: './public/favicon.ico'
+		})
+	]
+};
+
